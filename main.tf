99ca5ce76c58d544e0ae5361d8e9b22fb5bd7a2e
guyeisenkot <68634672+guyeisenkot@users.noreply.github.com>
2021-08-09 16:59:01 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
@@ -136,11 +135,42 @@ resource "azurerm_virtual_machine" "main" {
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
+  location            = "West Europe"
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "virtualNetwork1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
   }
 }
