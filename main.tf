36d1ca84182b4736ae2c45aa0e0a0cff21cae85c
guyeisenkot <68634672+guyeisenkot@users.noreply.github.com>
2021-08-09 16:48:56 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -92,6 +92,55 @@ resource "azurerm_sql_database" "example" {
   tags = {
     environment = "production"
     yor_trace = "fc8c2d7a-1997-4fc2-95c1-277cba5c2a39"
-    owner = "guy"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "geise-vm"
+  location              = "West US"
+  resource_group_name   = azurerm_resource_group.example.name
+  network_interface_ids = [azurerm_network_interface.example.name]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+    yor_trace = "fc8c2d7a-1997-4fc2-95c1-277cba5c2a66"
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    private_ip_address_allocation = "Dynamic"
   }
 }
